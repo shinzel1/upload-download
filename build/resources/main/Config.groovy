b17cda59dd72c6b69c8abfea68e75b33bcbe522b
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2021-09-16 19:37:31 +0530

    Add files via upload

---
+++ build/resources/main/Config.groovy
@@ -0,0 +1,11 @@
+environments {
+    development {
+        uploadFolder = "c:/temp/upload/"
+    }
+    test {
+        uploadFolder = "c:/temp/upload/"
+    }
+    production {
+        uploadFolder = "c:/temp/upload/"
+    }
+}
---
+++ src/test/groovy/gra/DocumentControllerSpec.groovy
@@ -0,0 +1,18 @@
+package gra
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class DocumentControllerSpec extends Specification implements ControllerUnitTest<DocumentController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
